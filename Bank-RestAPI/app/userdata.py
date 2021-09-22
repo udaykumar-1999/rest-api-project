46100b36f3f2967426d2b7bab7c0730156be2abf
udaykumar-1999 <74130632+udaykumar-1999@users.noreply.github.com>
2021-09-22 16:05:56 +0530

    Update userdata.py
    used random method for (acc_no), also added route for get request on all (/api/users)

--- Bank-RestAPI/app/userdata.py
+++ Bank-RestAPI/app/userdata.py
@@ -1,3 +1,7 @@
+import random
+import json
+from json import dumps
+from bson import json_util
 from app import app
 from flask import jsonify, make_response, request
 from config import client
@@ -9,11 +13,25 @@ collection = db['users']
 
 @app.route("/api/createuser", methods=['POST'])
 def create_user():
-    record = request.get_json()
-    if record:
-        collection.insert_one(record)
+    records = request.get_json()
+    if records:
+        for each_rec in records:
+            each_rec['acc_no'] = random.randrange(5000000, 7000000)
+        collection.insert_many(records)
         return make_response({"msg": "record inserted.."}, 201)
     return make_response(jsonify({"error": "record not inserted"}), 404)
+
+
+@app.route("/api/users")
+def users():
+    cursor_obj = collection.find()
+    record_str_lst = [json.dumps(doc, default=json_util.default) for doc in cursor_obj]
+    record_json_lst = []
+    for record in record_str_lst:
+        record_json_lst.append(json.loads(record))
+    if record_json_lst:
+        return make_response(jsonify(record_json_lst), 200)
+    return make_response(jsonify({"msg": "No users"}))
 
 
 @app.route("/api/getuser/<name>")
@@ -21,7 +39,7 @@ def get_user(name):
     record = collection.find_one({'name': name})
     if record:
         return make_response(jsonify(record), 200)
-    return make_response(jsonify({"error": "user not found"}), 404)
+    return make_response(jsonify({"error": "User not found"}), 404)
 
 
 @app.route("/api/updateuser/<name>", methods=['PUT'])
@@ -30,8 +48,8 @@ def update_user(name):
     exists = collection.find_one({'name': name})
     if exists:
         collection.update_one({'name': name}, {"$set": update_record})
-        return make_response(jsonify({"msg": "user updated.."}), 200)
-    return make_response(jsonify({"error": "user not found"}), 404)
+        return make_response(jsonify({"msg": "User updated.."}), 200)
+    return make_response(jsonify({"error": "User not found"}), 404)
 
 
 @app.route("/api/deleteuser/<name>", methods=['DELETE'])
@@ -39,5 +57,5 @@ def delete_user(name):
     exists = collection.find_one({'name': name})
     if exists:
         collection.delete_many({"name": name})
-        return make_response(jsonify({"msg": "deleted user..."}), 200)
-    return make_response(jsonify({"error": "user not found"}), 404)
+        return make_response(jsonify({"msg": "User deleted..."}), 200)
+    return make_response(jsonify({"error": "User not found"}), 404)
